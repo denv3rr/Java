77b7d4760444dc68e75b773046266425ba17c2d1
Denver Clark <91037215+denv3rr@users.noreply.github.com>
2022-10-27 17:20:05 -0500

    Add files via upload
    create player instances and object stream for each in order to copy entered info and display it in terminal

---
+++ SerializePlayerTest/Inventory.java
@@ -0,0 +1,16 @@
+import java.io.Serializable;
+
+public class Inventory implements Serializable {
+    private int slots;
+    private int usedSlots;
+
+    public Inventory(int slots, int usedSlots) {
+        this.slots = slots;
+        this.usedSlots = usedSlots;
+    }
+
+    public String toString() {
+        return String.format("Total Inventory Slots: %d\nInventory Slots Available: %d\n", this.slots,
+                this.slots - this.usedSlots);
+    }
+}
